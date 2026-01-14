de7bd6e28cd3111e7943db482088b18416151f21
jiaying <loh.ying0503@gmail.com>
2026-01-14 21:48:40 +0800

    Fix bug: Reset Password

--- Frontend/src/view/Auth/ForgotPassword.jsx
+++ Frontend/src/view/Auth/ForgotPassword.jsx
@@ -17,7 +17,7 @@ export function ForgotPassword() {
 
         try {
             const { error } = await supabase.auth.resetPasswordForEmail(email, {
-                redirectTo: 'http://localhost:5173/reset-password',
+                redirectTo: `${window.location.origin}/reset-password`,
             });
 
             if (error) throw error;
